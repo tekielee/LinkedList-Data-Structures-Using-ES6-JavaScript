7b2065176da4f62a8e31b8e727ac355f5c63b3e9
Cuong Le <tekielee@yahoo.com>
2020-08-06 21:20:17 -0700

    insertBefore function

--- linkedlist.js
+++ linkedlist.js
@@ -30,6 +30,56 @@ class LinkedList {
 		return true;
 	}
 	
+	insertAtFirst(data) {
+		let newNode = new ListNode(data);
+		
+		if(this.firstNode == null) {
+			this.firstNode = newNode;
+		} else {
+			let currentFirstNode = this.firstNode;
+			this.firstNode = newNode;
+			newNode.next = currentFirstNode;
+		}
+		
+		this.totalNode++;
+		return true;
+	}
+	
+	insertBefore(data, query) {
+		let newNode = new ListNode(data);
+
+		if(this.firstNode) {
+			let previous = null;
+			let currentNode = this.firstNode;
+			while(currentNode != null) {
+				if(currentNode.data == query) {
+					console.log('found')
+					newNode.next = currentNode;
+					previous.next = newNode;
+					this.totalNode++;
+					break;
+				}
+				previous = currentNode;
+				currentNode = currentNode.next;				
+			}
+		}
+	}
+	
+	search(data) {
+		if(this.totalNode) {
+			let currentNode = this.firstNode;
+			while(currentNode != null) {
+				if(currentNode == data) {
+					return currentNode;
+				}
+				
+				currentNode = currentNode.next;
+			}
+		}
+		
+		return false;
+	}
+	
 	display() {
 		console.log('Total book titles: ' + this.totalNode);
 		let currentNode = this.firstNode;
@@ -44,4 +94,10 @@ const bookTitles = new LinkedList();
 bookTitles.insert('Introduction to Algorithm');
 bookTitles.insert('Introduction to JavaScript and Data Structures');
 bookTitles.insert('Programming Intelligence');
+bookTitles.display();
+
+bookTitles.insertAtFirst('Mediawiki Administratrive tutorial guide')
+bookTitles.display();
+
+bookTitles.insertBefore('Introduction to Calculus', 'Programming Intelligence');
 bookTitles.display();
